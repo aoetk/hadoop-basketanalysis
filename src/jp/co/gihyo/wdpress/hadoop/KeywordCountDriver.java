207590055cd432876fa343b9a8de0105c223c54d
aoetk <aoe@nautilus-technologies.com>
2011-12-18 21:21:20 +0900

    注釈を追加。

--- src/jp/co/gihyo/wdpress/hadoop/KeywordCountDriver.java
+++ src/jp/co/gihyo/wdpress/hadoop/KeywordCountDriver.java
@@ -14,47 +14,49 @@ import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-
-/**
- *
- * @author aoetakashi
- */
-public class KeywordCountDriver extends Configured implements Tool {
+public class KeywordCountDriver extends Configured implements Tool { // ①
 
     @Override
     public int run(String[] args) throws Exception {
         if (args.length != 2) {
+            // ②
             System.out.printf("Usage: %s [generic options] <indir> <outdir>\n", getClass().getSimpleName());
             ToolRunner.printGenericCommandUsage(System.out);
             System.exit(-1);
         }
 
-        JobConf jobConf = new JobConf(getConf(), KeywordCountDriver.class);
+        JobConf jobConf = new JobConf(getConf(), KeywordCountDriver.class); // ③
         jobConf.setJobName("KeywordCount");
 
+        // ④
         FileInputFormat.addInputPath(jobConf, new Path(args[0]));
         FileOutputFormat.setOutputPath(jobConf, new Path(args[1]));
 
+        // ⑤
         jobConf.setMapperClass(KeywordMapper.class);
         jobConf.setReducerClass(SumReducer.class);
 
+        // ⑥
         jobConf.setMapOutputKeyClass(Text.class);
         jobConf.setMapOutputValueClass(IntWritable.class);
 
+        // ⑦
         jobConf.setOutputKeyClass(Text.class);
         jobConf.setOutputValueClass(IntWritable.class);
 
+        // ⑧
         JobClient.runJob(jobConf);
         return 0;
     }
 
     public static void main(String[] args) throws Exception {
+        // ⑨
         int exitCode = ToolRunner.run(new KeywordCountDriver(), args);
         System.exit(exitCode);
     }
 
 }
-class KeywordMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+class KeywordMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> { // ①
 
     private static final IntWritable ONE = new IntWritable(1);
 
@@ -62,17 +64,17 @@ class KeywordMapper extends MapReduceBase implements Mapper<LongWritable, Text, 
 
     @Override
     public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-        String line = value.toString();
-        String[] records = line.split("\t");
+        String line = value.toString(); // ②
+        String[] records = line.split("\t"); // ③
         if (records.length == 3) {
             keyword.set(records[2]);
-            output.collect(keyword, ONE);
+            output.collect(keyword, ONE); // ④
         }
     }
 
 }
 
-class SumReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+class SumReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> { // ①
 
     private IntWritable keywordCount = new IntWritable();
 
@@ -81,10 +83,10 @@ class SumReducer extends MapReduceBase implements Reducer<Text, IntWritable, Tex
             OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
         int count = 0;
         while (values.hasNext()) {
-            count += values.next().get();
+            count += values.next().get(); // ②
         }
         keywordCount.set(count);
-        output.collect(key, keywordCount);
+        output.collect(key, keywordCount); // ③
     }
 
 }
